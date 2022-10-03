23a01cd02e4fb7ed2802e66875ab0afcdebc0f8c
fatihozzgen <fatihozzgen@gmail.com>
2022-10-03 20:13:41 +0300

    3.2

--- breaking-bad/src/pages/Home.js
+++ breaking-bad/src/pages/Home.js
@@ -1,10 +1,50 @@
-import React from "react";
+import { useEffect } from "react";
+import Masonry from "react-masonry-css";
+import "./style.css";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacters } from "../redux/charactersSlice";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 function Home() {
-  const data = useSelector((state) => state.characters.items);
-  console.log(data);
-  return <div>Home</div>;
+  const characters = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error message={error} />;
+  }
+
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <img
+              alt={character.name}
+              src={character.img}
+              className="character"
+            />
+            <div className="charname">{character.name}</div>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Home;
--- breaking-bad/src/redux/charactersSlice.js
+++ breaking-bad/src/redux/charactersSlice.js
@@ -1,15 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCharacters = createAsyncThunk(
+  "characters/getCharacters",
+  async () => {
+    const REACT_APP_API_BASE_URL = "https://www.breakingbadapi.com/api";
+    const res = await axios(`${REACT_APP_API_BASE_URL}/characters?limit=12`);
+    return res.data;
+  }
+);
 
 export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
-    items: [
-      {
-        name: "fatih",
-      },
-    ],
+    items: [],
+    isLoading: false,
   },
   reducers: {},
+  extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [fetchCharacters.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
+  },
 });
 
 export default charactersSlice.reducer;
